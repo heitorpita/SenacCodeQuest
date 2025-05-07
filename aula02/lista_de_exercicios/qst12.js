81d02a8566451a342189ca6c4923409cae0afa4e
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-05-07 19:53:00 +0000

    last question

---
+++ aula02/lista_de_exercicios/qst12.js
@@ -0,0 +1,12 @@
+let salario_por_hora = 7.5;
+
+let horas_trabalhadas = 25 * 4;
+
+let salario_final = salario_por_hora * horas_trabalhadas;
+
+console.log(salario_final);
+
+
+
+
+// um trabalhador ganha por hora, guarde o valor da hora trabalhada e o numeros de hoas e depois calcule o salario final
