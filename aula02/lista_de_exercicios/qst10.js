26251b96c57ec1c197c0e6a2b81c33fb778b89ae
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-05-07 19:45:20 +0000

    atualizando +1 vez

---
+++ aula02/lista_de_exercicios/qst10.js
@@ -0,0 +1,6 @@
+let nome = "matue";
+let cidade = "FORTAL";
+
+console.log(`${nome} mora em ${cidade}`)
+
+//Guarde o nome da pessoa e a cidade onde ela vive e exiba
---
+++ aula02/lista_de_exercicios/qst11.js
@@ -0,0 +1,15 @@
+let distanciaEmKm = 100;
+let litros = 30
+let preco = 5.80
+
+let consumo = distanciaEmKm / litros
+let valor_gasto = consumo * preco
+console.log(valor_gasto.toFixed(2));
+
+
+
+
+
+//CALCULE O VALOR GASTO  EM GASOLINA EM UMA VIAGEM  
+// SABENDO A DISTANCIA EM KM, O CONSUMO DO CARRO (KM/LITRO) E O PREÇO DA GASOLINA
+// trajeto percorrido dividido pela quantidade de combustível utilizado
