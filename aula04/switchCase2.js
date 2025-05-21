af0dad1a61036de7407b768ed9e583bbce53343c
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-05-21 19:08:50 +0000

    feat: second questions about switch case

---
+++ aula04/switchCase2.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+const x = parseInt(prompt("Digite o primeiro número: "));
+const y = parseInt(prompt("Digite o segundo número: "));
+const z = parseInt(prompt("Digite o terceiro número: "));
+
+if (x >= 0 && x <= 10 && y >= 0 && y <= 10 && z >= 0 && z <= 10) {
+    const escolha = parseInt(prompt("Escolha a média (1: aritmética, 2: geométrica, 3: ponderada): "));
+    switch (escolha) {
+        case 1:
+            console.log(`Média aritmética: ${((x + y + z) / 3)}`);
+            break;
+        case 2:
+            console.log(`Média geométrica: ${Math.cbrt(x * y * z)}`);
+            break;
+        case 3:
+            console.log(`Média ponderada: ${((x * 2 + y * 3 + z * 5) / 10)}`);
+            break;
+        default:
+            console.log("Opção inválida, escolha 1, 2 ou 3");
+            break;
+    }
+} else {
+    console.log("Repita, os números não são válidos");
+}
