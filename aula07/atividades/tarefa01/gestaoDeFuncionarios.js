2a6d2ffeec4d405fd9b47056badfebdb6a66c749
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-06-04 18:51:40 +0000

    feat: mais atividades

--- aula07/atividades/tarefa01/gestaoDeFuncionarios.js
+++ aula07/atividades/tarefa01/gestaoDeFuncionarios.js
@@ -8,9 +8,9 @@ Digite a opção desejada:
 2 - Cálculo de aumento salarial
 3 - Verificar situação do funcionário
 4 - Sair
--------------------------------------------`)
+------------------------------------------- \n`)
 
-codigo = parseInt(prompt("Digite a opção desejada (1-Cálculo de imposto, 2-Cálculo de aumento salarial, 3-Verificar situação do funcionário, 4-Sair): "));
+codigo = parseInt(prompt("Digite a opção desejada: "));
 
 if (codigo === 4) break;
 
@@ -28,9 +28,31 @@ switch (codigo) {
         } else {
              imposto = (salario * 15) / 100
              console.log(`O IMPOSTO A SER PAGO É NO VALOR DE ${imposto}`)
-        }
+        };
     case 2:
-        
+        if (salario < 450) {
+            let aumento = salario + 100
+            console.log(`AUMENTE DE 100 TOTAL DE ${aumento}`);
+            
+        } else if (salario < 750) {
+            let aumento = salario + 75
+             console.log(`AUMENTE DE 75 TOTAL DE ${aumento}`);
+        } else if (salario <= 1500 ) {
+            let aumento = salario + 50
+            console.log(`AUMENTE DE 50 TOTAL DE ${aumento}`);
+            
+        }else {
+            aumento = salario + 25
+             console.log(`AUMENTE DE 25 TOTAL DE ${aumento}`);
+        };
+    case 3:
+        if (salario <= 700 ) {
+            console.log("MAL REMUNERADO");
+            
+        } else {
+            console.log("BEM REMUNERADO");
+            
+        }
 }
 
 
