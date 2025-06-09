104f688749846fe2a60cbd1e9bd8e233c1d19342
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-06-09 19:11:58 +0000

    .

--- aula09/atividade_de_classe/10.js
+++ aula09/atividade_de_classe/10.js
@@ -1,4 +1,4 @@
-// Array for positive/negative check
+
 let numeros = [-4, 3, 0, -1, 10, 7];
 
 // 1. Check positive/negative numbers
@@ -10,14 +10,14 @@ for (let i = 0; i < numeros.length; i++) {
     }
 }
 
-// 2. List even numbers from 1 to 10
+
 for (let i = 1; i <= 10; i++) {
     if (i % 2 === 0) {
         console.log(`${i} é par`);
     }
 }
 
-// 3. List odd numbers from 1 to 20
+
 for (let i = 1; i <= 20; i++) {
     if (i % 2 === 1) {
         console.log(`${i} é ímpar`);
