90c81064aac7996c7f1f8605d20141011b6ac546
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-06-11 18:18:13 +0000

    feat: explicaçoes + atividades

---
+++ aula10/atividades/01.js
@@ -0,0 +1,8 @@
+let camisa = ["camisa preta", "camisa azul", "camisa rosa"]
+let calca = ["calca preta", "calca roxa" , "calca verde-limao"]
+
+
+for (i = 0; i < camisa.length; i++) {
+    for (j = 0; j < camisa.length)
+    
+}
