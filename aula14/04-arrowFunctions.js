dfdff86c4793366e4a8406902b5b5dfe79c1b6ce
heitorpita <140120527+heitorpita@users.noreply.github.com>
2025-06-25 18:24:08 +0000

    feat: funcs

--- aula14/04-arrowFunctions.js
+++ aula14/04-arrowFunctions.js
@@ -9,3 +9,10 @@ console.log(resultado);
 // let dobro = (x) => x * 2; // Versão com parênteses
 // let dobro = (x) => { return x * 2; } // Versão com chaves
 // let dobro = x => { return x * 2; } // Versão com chaves e sem parênteses
+
+function exibirArray(array) {
+    array.forEach(item => console.log(item));
+}
+let games = ["The Last of Us", "God of War", "Horizon Zero Dawn"];
+exibirArray(games);
+exibirArray(["Banana", "Maçã", "Laranja"]);
