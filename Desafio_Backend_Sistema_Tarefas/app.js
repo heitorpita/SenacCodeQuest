063bae6c36dee748ea12020fcecd09af0dadeb16
heitorpita <heitorpita96@gmail.com>
2025-11-22 16:48:18 -0300

    feat: Ajustando app.js e routes

--- Desafio_Backend_Sistema_Tarefas/app.js
+++ Desafio_Backend_Sistema_Tarefas/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import "dotenv/config";
-import UsuarioRoutes from "./src/routes/Usuarios/UsuarioRoutes.js"
+import Routes from "./src/routes/Routes.js"
 
 const app = express();
 const port = process.env.PORT
@@ -8,6 +8,6 @@ const port = process.env.PORT
 
 app.use(express.json())
 
-app.use("/Usuarios", UsuarioRoutes);
+app.use("/Rotas", Routes);
 
 app.listen(port, () => { console.log(`Aplicacao rodando na porta http://localhost:${port}`) });
--- Desafio_Backend_Sistema_Tarefas/src/routes/Routes.js
+++ Desafio_Backend_Sistema_Tarefas/src/routes/Routes.js
@@ -1,21 +1,22 @@
-import TarefaController from "../controllers/Tarefas/TarefaController.js";
-import UsuarioController from "../controllers/Usuarios/UsuarioController.js";
-import { authenticator } from "../middlewares/authMiddleware.js";
 import express from "express";
+import UsuarioController from "../controllers/Usuarios/UsuarioController.js";
+import TarefaController from "../controllers/Tarefas/TarefaController.js";
+import { authenticator } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.post("/login", UsuarioController.login);
-router.post("/", UsuarioController.criar)
+// Usuários (públicas)
+router.post("/auth/login", UsuarioController.login);
+router.post("/auth/registro", UsuarioController.criar);
 
-//rota privada
-router.get("/perfil/dados", authenticator, UsuarioController.perfil);
+// Perfil (privada)
+router.get("/auth/perfil", authenticator, UsuarioController.perfil);
 
-//tarefas
-
-router.get("/listar", authenticator, TarefaController.listar)
-router.post("/criarTarefa", authenticator, TarefaController.criar)
-router.get("/listar:id", authenticator, TarefaController.buscarPorId)
-router.put("/listar:id", authenticator, TarefaController.atualizar)
-router.delete("/listar:id:", authenticator, TarefaController.deletar)
+// Tarefas (privadas)
+router.get("/tarefas", authenticator, TarefaController.listar);
+router.post("/tarefas", authenticator, TarefaController.criar);
+router.get("/tarefas/:id", authenticator, TarefaController.buscarPorId);
+router.put("/tarefas/:id", authenticator, TarefaController.atualizar);
+router.delete("/tarefas/:id", authenticator, TarefaController.deletar);
 
 export default router;
